29003a745bb013bebc6e87e15220bd19f22da78e
rashifmalikilyasa <malikutama30@gmail.com>
2019-08-14 15:35:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intentproject/IntentActivity.java
@@ -0,0 +1,13 @@
+package com.example.intentproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class IntentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+}
